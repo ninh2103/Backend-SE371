6423aa889745284d27ed215d3a1d059f214650df
Nguyen Duc Ninh <nguyenducninh210303@gmail.com>
2024-05-18 16:02:11 +0700

    update 18/5/2024

--- src/routes/medias.routes.ts
+++ src/routes/medias.routes.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express'
 import { uploadImageController } from '~/controllers/medias.controller'
 import { uploadVideoController } from '~/controllers/users.controllers'
-import { acceptValidator, verifiedUserValidator } from '~/middlewares/users.middleware'
+import { accessTokenValidator, verifiedUserValidator } from '~/middlewares/users.middleware'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 export const mediasRouter = Router()
-mediasRouter.post('/upload-image', acceptValidator, verifiedUserValidator, wrapRequestHandler(uploadImageController))
-mediasRouter.post('/upload-video', acceptValidator, verifiedUserValidator, wrapRequestHandler(uploadVideoController))
+mediasRouter.post(
+  '/upload-image',
+  accessTokenValidator,
+  verifiedUserValidator,
+  wrapRequestHandler(uploadImageController)
+)
+mediasRouter.post(
+  '/upload-video',
+  accessTokenValidator,
+  verifiedUserValidator,
+  wrapRequestHandler(uploadVideoController)
+)
